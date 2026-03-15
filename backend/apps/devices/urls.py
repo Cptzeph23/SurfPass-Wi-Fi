d1c4425cbd155595aeeca97d48150c04967eeee2
Cptzeph23 <asekazephaniah@gmail.com>
2026-03-15 17:05:01 +0300

    urls placeholders

--- backend/apps/devices/urls.py
+++ backend/apps/devices/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 
-app_name = "wifi_portal"
+app_name = "wifi_devices"
 urlpatterns = []
---
+++ backend/apps/sessions/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path
+
+app_name = "wifi_sessions"
+urlpatterns = []
