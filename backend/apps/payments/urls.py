bd621ea264553cf000b94f6c2606d73da7126a54
Cptzeph23 <asekazephaniah@gmail.com>
2026-03-16 16:11:45 +0300

    payment urls

--- backend/apps/payments/urls.py
+++ backend/apps/payments/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
+from . import views
 
 app_name = "wifi_payments"
-urlpatterns = []
+
+urlpatterns = [
+    path("initiate/", views.initiate_payment, name="initiate"),
+    path("status/<str:payment_id>/", views.payment_status, name="status"),
+    path("mpesa/callback/", views.mpesa_callback, name="mpesa_callback"),
+]
