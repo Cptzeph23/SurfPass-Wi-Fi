444ab1f933cb571bd3cfe19477b6c6243b395821
Cptzeph23 <asekazephaniah@gmail.com>
2026-03-16 16:10:19 +0300

    portal urls

--- backend/apps/portal/urls.py
+++ backend/apps/portal/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
+from . import views
 
 app_name = "wifi_portal"
-urlpatterns = []
+
+urlpatterns = [
+    path("status/", views.check_status, name="check_status"),
+    path("packages/", views.list_packages, name="list_packages"),
+    path("voucher/redeem/", views.redeem_voucher, name="redeem_voucher"),
+]
