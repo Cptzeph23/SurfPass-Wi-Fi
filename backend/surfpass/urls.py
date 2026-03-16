8f9c56590d2d28511b5fd0f63d42764b3a0f406e
Cptzeph23 <asekazephaniah@gmail.com>
2026-03-16 16:15:02 +0300

    update root urls

--- backend/surfpass/urls.py
+++ backend/surfpass/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/v1/portal/", include("apps.portal.urls")),
     path("api/v1/payments/", include("apps.payments.urls")),
     path("api/v1/sessions/", include("apps.sessions.urls")),
